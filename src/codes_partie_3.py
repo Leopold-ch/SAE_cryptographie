0767c3250d8d509ced21d2c1fee6a3c7f27af06d
DanCoquerant <134592350+DanCoquerant@users.noreply.github.com>
2023-12-22 10:23:13 +0100

    Ajout filtre port

--- src/codes_partie_3.py
+++ src/codes_partie_3.py
@@ -11,3 +11,19 @@ def filtre_UDP(paquets):
         list: liste des paquet ayant une sous couche UDP
     """
     return [p for p in paquets if UDP in p]
+
+def filtre_port(paquets_UDP, port_recherche):
+    """fonction permettant de ne conserver que les paquets
+    utilisant un port donné
+
+    Args:
+        paquets_UDP (scapy.plist.PacketList): liste de paquets ayant une sous couche UDP
+        port_recherche (int): port de communication recherché
+
+    Returns:
+        list: liste des paquets passant par le port donné
+    """
+    try:
+        return [p for p in paquets_UDP if p[UDP].dport == port_recherche]
+    except IndexError:
+        print("La liste des paquets en paramètre est incorrecte !")
